906c1655d7b19390e67b43fec356a0ce3664769c
andre <andrenovais@usp.br>
2020-03-07 14:37:55 -0300

    testando_tempo_de_implementacao

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js Ap"/>
+    <grafico/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js Ap"/>-->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import grafico from './components/grafico.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    grafico
   }
 }
 </script>
---
+++ src/components/grafico.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul>
+            <li vfor="json in info">
+                <h1>{{ json }}</h1>
+            </li>
+        </ul>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            info:[]
+        }
+    },
+    created(){
+        this.$http.get("https://projeto-acme.herokuapp.com/financeiro")
+            .then(res => res.json())
+            .then(info => this.info = info);
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
