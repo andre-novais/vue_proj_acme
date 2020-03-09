c4bf85ac7cfe883a843966be471c09aa096e4aa6
andre <andrenovais@usp.br>
2020-03-09 09:35:55 -0300

    teste_get

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <grafico msg="to_aqui"/>
-    <!--<HelloWorld msg="Welcome to Your Vue.js Ap"/>-->
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vusex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <div>
+      <Graphs/>
+    </div>
   </div>
 </template>
 
+
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
-import grafico from './components/grafico.vue'
+import Graphs from './components/Graphs.vue'
 
 export default {
   name: 'App',
   components: {
     //HelloWorld,
-    Graphs
+    "Graphs":Graphs
   }
 }
 </script>
